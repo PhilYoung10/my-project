b1fa32b84a9a40fbc315221e741d12d2f76dd79c
Phil <PhilYoung10@163.com>
2019-05-14 00:23:03 +0800

    201905140006:初步尝试整合Vue与BootStrap

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,37 @@
 <!--    <dataRelate></dataRelate>-->
 <!--  </div>-->
 
+<div>
   <div class="container">
-    <div id="functionMenu">
-      <div id="cabinetLayout">
+    <div id="functionMenu" class="row-fluid">
+      <div id="cabinetLayout" class="span4">
         <img src="./assets/logo.png">机柜布局图
       </div>
-      <div id="addProperty">
+      <div id="addProperty" class="span4">
         <img src="./assets/logo.png">增加设备
       </div>
-      <div id="queryProperty">
+      <div id="queryProperty" class="span4">
         <img src="./assets/logo.png">查询设备
       </div>
     </div>
   </div>
+
+  <div class="row-fluid">
+    <div class="span12">
+      Fluid 12
+      <div class="row-fluid">
+        <div class="span6">
+          Fluid 6
+          <div class="row-fluid">
+            <div class="span6">Fluid 6</div>
+            <div class="span6">Fluid 6</div>
+          </div>
+        </div>
+        <div class="span6">Fluid 6</div>
+      </div>
+    </div>
+  </div>
+</div>
 
 </template>
 
