1be018ebadbb1ed0366e5330142ab82a34968105
Phil <PhilYoung10@163.com>
2019-05-13 17:52:42 +0800

    201905130005:Starting to design the Home page.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,58 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <helloVue></helloVue>
-<!--    <router-view/>-->
-    <dataRelate></dataRelate>
+<!--  <div id="app">-->
+<!--    <img src="./assets/logo.png">-->
+<!--    <helloVue></helloVue>-->
+<!--&lt;!&ndash;    <router-view/>&ndash;&gt;-->
+<!--    <dataRelate></dataRelate>-->
+<!--  </div>-->
+
+  <div class="container">
+    <div id="functionMenu">
+      <div id="cabinetLayout">
+        <img src="./assets/logo.png">机柜布局图
+      </div>
+      <div id="addProperty">
+        <img src="./assets/logo.png">增加设备
+      </div>
+      <div id="queryProperty">
+        <img src="./assets/logo.png">查询设备
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
+  // export default {
+  //   name: 'App'
+  // }
+
   // 导入组件
+  // import HelloVue from './components/HelloVue'
+  // import DataRelate from './components/DataRelate'
+  // export default {
+  //   name: 'app',
+  //   components: {
+  //     HelloVue,
+  //     DataRelate
+  //   }
+  // }
 
-  import HelloVue from './components/HelloVue'
-  import DataRelate from './components/DataRelate'
-  export default {
-    name: 'app',
-    components: {
-      HelloVue,
-      DataRelate
-    }
-  }
 
 
-// export default {
-//   name: 'App'
-// }
+
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
+
+
 </style>
