628171f01f71be7dbacfd7b19021d0c98a849461
Phil <PhilYoung10@163.com>
2019-05-27 17:45:18 +0800

    Phil-20190527-0011:Login Module

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,20 @@
       <div v-if="isLogin">
         <div class="logout">
           <p style="display: inline-block">{{user.userName}}</p>
-<!--          <img src="static/icon/wulumuqishigongandashujuguanlipingtai-ico-.png">-->
+          <!--          <img src="static/icon/wulumuqishigongandashujuguanlipingtai-ico-.png">-->
           <Button v-on:click="logout" type="error" size="small">退出登录</Button>
         </div>
         <div class="functionMenu">
           <Divider>功能菜单</Divider>
-          <div id="addProperty" class="function"  v-on:click="addProperty">
+          <div id="addProperty" class="function" v-on:click="addProperty">
             <img src="/static/icon/plus_128px_1160196_easyicon.net.png">
             <p>增加设备</p>
           </div>
-          <div id="queryProperty" class="function"  v-on:click="queryProperty">
+          <div id="queryProperty" class="function" v-on:click="queryProperty">
             <img src="/static/icon/search_127.65957446809px_1138927_easyicon.net.png">
             <p>查询设备</p>
           </div>
-          <div id="cabinetLayout" class="function"  v-on:click="cabinetLayout">
+          <div id="cabinetLayout" class="function" v-on:click="cabinetLayout">
             <img src="/static/icon/software_layout_header_columns_128.48301886792px_1183030_easyicon.net.png">
             <p>机柜布局图</p>
           </div>
@@ -30,7 +30,7 @@
       </div>
       <div v-else>
         <div class="login">
-          <Divider>请登录</Divider>
+          <Divider>Please Signin</Divider>
 
           <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
             <FormItem prop="user">
@@ -49,14 +49,14 @@
             </FormItem>
           </Form>
 
-          <div class="loginForm">
-            <Input v-model="user.userName" id="userName" placeholder="用户名">
-            <span slot="prepend">用户名:</span>
-            <span slot="append">@aviva-cofco.com.cn</span>
-            </Input>
-            <Input type="password" v-model="user.password" id="password" placeholder="密码" v-on:keyup.enter="login" style="margin-top: 10px"/>
-            <Button v-on:click="login" type="primary" size="small" style="margin-top: 10px">登录</Button>
-          </div>
+          <!--          <div class="loginForm">-->
+          <!--            <Input v-model="user.userName" id="userName" placeholder="用户名">-->
+          <!--            <span slot="prepend">用户名:</span>-->
+          <!--            <span slot="append">@aviva-cofco.com.cn</span>-->
+          <!--            </Input>-->
+          <!--            <Input type="password" v-model="user.password" id="password" placeholder="密码" v-on:keyup.enter="login" style="margin-top: 10px"/>-->
+          <!--            <Button v-on:click="login" type="primary" size="small" style="margin-top: 10px">登录</Button>-->
+          <!--          </div>-->
         </div>
       </div>
     </div>
@@ -76,18 +76,20 @@
           status: "0"
         },
         isLogin: Global.loginStatus,
+        checkUser: Global.checkUser(),
 
         formInline: {
-          user: '',
-          password: ''
+          user: 'Phil_Yang',
+          password: 'password',
+          status: "0"
         },
         ruleInline: {
           user: [
-            { required: true, message: 'Please fill in the user name', trigger: 'blur' }
+            {required: true, message: 'Please fill in the user name', trigger: 'blur'}
           ],
           password: [
-            { required: true, message: 'Please fill in the password.', trigger: 'blur' },
-            { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
+            {required: true, message: 'Please fill in the password.', trigger: 'blur'},
+            {type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur'}
           ]
         }
       }
@@ -98,27 +100,31 @@
         this.$refs[name].validate((valid) => {
           if (valid) {
             this.$Message.success('Success!');
+            this.formInline.status = "1";
+            Global.loginStatus = 1;
+            this.isLogin = 1;
           } else {
-            this.$Message.error('Fail!');
+            this.$Message.error('User name or password is illegal!');
           }
         })
       },
 
-      login: function () {
-        this.user.status = "1";
-        Global.loginStatus = 1;
-        this.isLogin = 1;
-      },
-      logout:function () {
+      // login: function () {
+      //   this.user.status = "1";
+      //   Global.loginStatus = 1;
+      //   this.isLogin = 1;
+      // },
+      logout: function () {
         this.user.status = "0";
         Global.loginStatus = 0;
         this.isLogin = 0;
+        this.$Message.error('Logout!');
       },
       addProperty: function () {
 
       },
       queryProperty: function () {
-        
+
       },
       cabinetLayout: function () {
 
@@ -144,7 +150,7 @@
   }
 
   .systemName {
-    color: #371250  ;
+    color: #371250;
     font-size: 40px;
   }
 
--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 export default {
   themeColor: '#371250',
-  loginStatus: 0
-  // loginStatus: function () {
-  //   return 0
-  // }
+  loginStatus: 0,
+  checkUser: function () {
+    return (0)
+  }
 }
